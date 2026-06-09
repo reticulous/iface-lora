0781f458103b42079422a33d54a57f9f0754eb1e
Rop Gonggrijp <rop@gonggri.jp>
2026-06-09 15:07:51 +0000

    browser: move LoRa iface menu to settings/mesh/interfaces
    Signed-off-by: Rop Gonggrijp <rop@gonggri.jp>

--- browser/src/modules/lora.ts
+++ browser/src/modules/lora.ts
@@ -2,5 +2,7 @@ import { useMenuStore } from 'spangap-browser/stores/menu'
 import LoraPanel from '../panels/LoraPanel.vue'
 
 export function registerLora() {
-  useMenuStore().register('settings/reticulum/transports/lora', 'LoRa', { type: 'panel', component: LoraPanel })
+  const menu = useMenuStore()
+  menu.setMenu('settings/mesh/interfaces', { label: 'RNS Interfaces', placement: 2 })
+  menu.register('settings/mesh/interfaces/lora', 'LoRa', { type: 'panel', component: LoraPanel })
 }
